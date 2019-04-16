83199ab44d74d343790b26dcfd994d1c5cf2db64
boonyachengdu <boonyachengdu@gmail.com>
2019-04-16 12:47:21 +0800

    Add WKT format description.

--- src/com/boonya/gis/algorithm/WKTUtil.java
+++ src/com/boonya/gis/algorithm/WKTUtil.java
@@ -12,6 +12,19 @@ import java.util.Map;
  * @package  com.boonya.gis.algorithm
  * @filename WKTUtil.java
  * @date     2019年4月15日 下午5:55:59
+ * 
+ * @WKTformat
+ * POINT(6 10)<br/>
+ * LINESTRING(3 4,10 50,20 25)<br/>
+ * POLYGON((1 1,5 1,5 5,1 5,1 1),(2 2,2 3,3 3,3 2,2 2))<br/>
+ * MULTIPOINT(3.5 5.6, 4.8 10.5)<br/>
+ * MULTILINESTRING((3 4,10 50,20 25),(-5 -8,-10 -8,-15 -4))<br/>
+ * MULTIPOLYGON(((1 1,5 1,5 5,1 5,1 1),(2 2,2 3,3 3,3 2,2 2)),((6 3,9 2,9 4,6 3)))<br/>
+ * GEOMETRYCOLLECTION(POINT(4 6),LINESTRING(4 6,7 10))<br/>
+ * POINT ZM (1 1 5 60)<br/>
+ * POINT M (1 1 80)<br/>
+ * POINT EMPTY<br/>
+ * MULTIPOLYGON EMPTY<br/>
  */
 public class WKTUtil {
 	
